c2d2625b24c78e5090d4e18ecaaa5c0e5deb716b
Giorma <giomaisuradze30@gmail.com>
2022-10-14 16:40:34 +0400

    First-Commit

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ List = []
 List_Size = int(input("Enter number of elements in list: "))
 
 for i in range(1, List_Size + 1):
-    elements_in_list = int(input("Enter element: "))
+    elements_in_list = float(input("Enter element: "))
     List.append(elements_in_list)
 
 print("Largest element is: ", max(List))
